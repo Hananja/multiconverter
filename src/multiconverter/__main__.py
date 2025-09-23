c6e4b3056e96eed8dbdb025feab6bad5168126cc
E. Loos <22101627+Hananja@users.noreply.github.com>
2025-09-23 08:53:01 +0200

    fix for pipx

--- src/multiconverter/__main__.py
+++ src/multiconverter/__main__.py
@@ -1,7 +1,11 @@
 # src/mein_projekt/__main__.py
 import sys
 
-from multiconverter.converter5 import main
+from multiconverter.converter5 import main as converter_main
+
+def main():
+    converter_main(sys.argv)
+
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
